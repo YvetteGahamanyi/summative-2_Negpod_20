5c52527f84d21d3385f6a56404713ef403de26a4
irebealice <i.alice@alustudent.com>
2024-02-22 12:00:59 -0800

    update

---
+++ backup-Negpod_20.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#configuration 
+
+HOST="64293e56bc62.3a2627c1.alu-cod.online"
+USERNAME="64293e56bc62"
+PASSWORD="328d3b338a4ced526c9a"
+
+
+# Destination location on the remote server
+DESTINATION="/summative/1023-2024j"
+
+DIRECTORY="negpod_20-q1"
+
