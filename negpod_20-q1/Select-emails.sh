3cb3ac678fe3b89e558a2bc7b32a72dbf6aefa3e
igiranezah59 <higiranez@gmail.com>
2024-02-22 21:14:48 +0200

    feat: add select emails function

--- negpod_20-q1/Select-emails.sh
+++ negpod_20-q1/Select-emails.sh
@@ -6,3 +6,9 @@ student_emails="student-emails.txt"
 # Header for the table
 echo "No |  Email" > student-emails.txt
 
+# Function to select emails of students, sort them alphabetically, and save them to student-emails.txt
+selectEmails() {
+    grep -oP '\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b' $students_file | sort | awk '{print NR, " | ", $1}' >> $student_emails
+    echo "Emails of students have been selected, sorted and saved to $student_emails"
+    cat $student_emails
+}
