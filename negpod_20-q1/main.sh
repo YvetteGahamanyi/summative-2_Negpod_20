d6dad3e4467849f21251431f6a90d8ca86f271f3
MikeManzi <manzimike378@gmail.com>
2024-02-22 20:25:28 +0200

    add application menu

--- negpod_20-q1/main.sh
+++ negpod_20-q1/main.sh
@@ -5,4 +5,16 @@ email_width=20
 age_width=3
 id_width=3
 
+while true; do
+    echo "------------------"
+    echo "ALU Registration System"
+    echo "------------------"
+    echo "1. Create student record"
+    echo "2. View all students"
+    echo "3. Delete student record"
+    echo "4. Update student record"
+    echo "5. Select student emails"
+    echo "6. Exit"
+
+    read -p "Enter your choice: " choice
 
